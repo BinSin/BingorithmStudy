7894e26515dc29859b8d711d4fc0715998277044
BinSin <binyong318@gmail.com>
2022-03-02 22:41:36 +0900

    다트 게임

--- programmers/src/practice/kakao/level1/Solution07.java
+++ programmers/src/practice/kakao/level1/Solution07.java
@@ -1,11 +1,43 @@
 package practice.kakao.level1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution07 { // 다트 게임
 
     public static int solution(String dartResult) {
-        int answer = 0;
-
-        return answer;
+        List<Integer> list = new ArrayList<>();
+        int number = 0;
+        int index = 0;
+        for (int i=0; i<dartResult.length(); i++) {
+            char c = dartResult.charAt(i);
+            if (c >= '0' && c <= '9') {
+                number = c - '0';
+                if (number == 1) {
+                    if(dartResult.charAt(i + 1) == '0') {
+                        number = 10;
+                        i++;
+                    }
+                }
+            } else if (c == 'S') {
+                index++;
+                list.add(number);
+            } else if (c == 'D') {
+                index++;
+                list.add((int) Math.pow(number, 2));
+            } else if (c == 'T') {
+                index++;
+                list.add((int) Math.pow(number, 3));
+            } else if (c == '*') {
+                list.set(index - 1, list.get(index - 1) * 2);
+                if(index > 1) {
+                    list.set(index - 2, list.get(index - 2) * 2);
+                }
+            } else if (c == '#') {
+                list.set(index - 1, list.get(index - 1) * (-1));
+            }
+        }
+        return list.stream().mapToInt(Integer::valueOf).sum();
     }
 
     public static void main(String[] args) {

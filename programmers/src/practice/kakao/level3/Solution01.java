07bde3a476120179ff721e8c8161fc3d933c8567
binym <binym@humuson.com>
2022-03-04 18:35:01 +0900

    카카오 프렌즈 컬러링북 - BFS

--- programmers/src/practice/kakao/level3/Solution01.java
+++ programmers/src/practice/kakao/level3/Solution01.java
@@ -42,7 +42,7 @@ public class Solution01 {
 
     public static void main(String[] args) {
         int n = 6;
-        int[][] vertex = {{3,6},{4,3},{3,2},{1,3},{1,2},{2,4},{5,2}};
+        int[][] vertex = {{3, 6}, {4, 3}, {3, 2}, {1, 3}, {1, 2}, {2, 4}, {5, 2}};
         System.out.println(solution(n, vertex));
     }
 }

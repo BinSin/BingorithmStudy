7f60a2e539b40c830ec68468d59c2a710005165c
BinSin <binyong318@gmail.com>
2022-03-06 02:35:52 +0900

    메뉴 리뉴얼 - 조합 (combination) - 손님의 메뉴에서 만들 수 있는 조합 갯수들을 구한 뒤 최대값 추출

--- programmers/src/practice/kakao/level2/Solution05.java
+++ programmers/src/practice/kakao/level2/Solution05.java
@@ -5,80 +5,55 @@ https://programmers.co.kr/learn/courses/30/lessons/72411
 
 package practice.kakao.level2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Solution05 {
 
+    private static List<String> answerList = new ArrayList<>();
     private static Map<String, Integer> map;
     private static boolean[] visited;
     private static char[] answer;
-    private static Character[] menu; // 요리 가능한 메뉴들
-    private static int max = 0;
 
-    public static void check(String str, String[] orders) {
-        char[] cArr = str.toCharArray();
-        for (String order : orders) {
-            int count = 0;
-            for (char c : cArr) {
-                if (order.contains(String.valueOf(c)))
-                    count++;
-            }
-            if (count == str.length()) {
-                int nextValue = map.getOrDefault(str, 0) + 1;
-                map.put(str, nextValue);
-                max = Math.max(nextValue, max);
-            }
-        };
-
-    }
-
-    public static void combination(int depth, int r, int n, int menuCount, String[] orders) {
-        if (depth == r) {
-            check(String.valueOf(answer), orders);
+    public static void combination(int depth, int c, int n, char[] order) {
+        if (depth == c) {
+            String ans = String.valueOf(answer);
+            map.put(ans, map.getOrDefault(ans, 0) + 1);
             return;
         }
-        for (int i=n; i<menuCount; i++) {
+        for (int i=n; i<order.length; i++) {
             if(!visited[i]) {
                 visited[i] = true;
-                answer[depth] = menu[i];
-                combination(depth + 1, r, i, menuCount, orders);
+                answer[depth] = order[i];
+                combination(depth + 1, c, i + 1, order);
                 visited[i] = false;
             }
         }
     }
 
     public static String[] solution(String[] orders, int[] course) {
-        Set<Character> set = new HashSet<>();
-        for(String order : orders) {
-            char[] cArr = order.toCharArray();
-            for(char c : cArr) set.add(c);
-        }
-        menu = set.toArray(Character[]::new);
-
-        List<String> finalList = new ArrayList<>();
-        int menuCount = menu.length;
         for(int c : course) {
             map = new HashMap<>();
-            visited = new boolean[menuCount];
-            answer = new char[c];
-            max = 0;
-            combination(0, c, 0, menuCount, orders);
+            for(String order : orders) {
+                char[] sortOrder = order.toCharArray();
+                Arrays.sort(sortOrder);
+                int len = order.length();
+                visited = new boolean[len];
+                answer = new char[c];
+                combination(0, c, 0, sortOrder);
+            }
+
+            AtomicInteger max = new AtomicInteger();
+            map.forEach((k, v) -> {
+                if(v > 1) max.set(Math.max(max.get(), v));
+            });
 
             map.forEach((k, v) -> {
-                if(v > 1 && v == max) {
-                    finalList.add(k);
-                }
+                if(v == max.get()) answerList.add(k);
             });
         }
-        finalList.sort(String::compareTo);
-
-        return finalList.toArray(String[]::new);
+        Collections.sort(answerList);
+        return answerList.toArray(String[]::new);
     }
 
     public static void main(String[] args) {
@@ -86,11 +61,13 @@ public class Solution05 {
         int[] course1 = {2,3,4};
         Arrays.stream(solution(orders1, course1)).map(e -> e + " ").forEach(System.out::print);
         System.out.println();
+        answerList.clear();
 
         String[] orders2 = new String[] {"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"};
         int[] course2 = {2,3,5};
         Arrays.stream(solution(orders2, course2)).map(e -> e + " ").forEach(System.out::print);
         System.out.println();
+        answerList.clear();
 
         String[] orders3 = new String[] {"XYZ", "XWY", "WXA"};
         int[] course3 = {2,3,4};

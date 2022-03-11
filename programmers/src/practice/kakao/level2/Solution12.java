d4243423623bea10c9f419dbdb1040f971391d34
binym <binym@humuson.com>
2022-03-11 18:42:57 +0900

    후보키 (진행중)

---
+++ programmers/src/practice/kakao/level2/Solution12.java
@@ -0,0 +1,20 @@
+/*
+후보키
+https://programmers.co.kr/learn/courses/30/lessons/42890
+ */
+
+package practice.kakao.level2;
+
+public class Solution12 {
+
+    public static int solution(String[][] relation) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[][] relation = {{"100","ryan","music","2"},{"200","apeach","math","2"},{"300","tube","computer","3"},{"400","con","computer","4"},{"500","muzi","music","3"},{"600","apeach","music","2"}};
+        System.out.println(solution(relation));
+    }
+
+}

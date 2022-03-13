e8fe424c2b287699bcb9d650cd3a81418651dcad
binym <binym@humuson.com>
2022-03-13 14:09:18 +0900

    후보키 - 비트 연산 활용

--- programmers/src/practice/kakao/level2/Solution12.java
+++ programmers/src/practice/kakao/level2/Solution12.java
@@ -5,11 +5,41 @@ https://programmers.co.kr/learn/courses/30/lessons/42890
 
 package practice.kakao.level2;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Solution12 {
 
+    // 최소성을 만족하는지 검사
+    private static boolean check(List<Integer> candidateKeyList, int b) {
+        for (Integer key : candidateKeyList) {
+            if ((key & b) == key) return false;
+        }
+        return true;
+    }
+
     public static int solution(String[][] relation) {
-        int answer = 0;
-        return answer;
+        int n = relation.length;
+        int m = relation[0].length;
+
+        List<Integer> candidateKeyList = new ArrayList<>();
+        for (int b=0; b<(1<<m); b++) {
+            if (!check(candidateKeyList, b)) continue;
+
+            Set<String> set = new HashSet<>();
+            for (int i=0; i<n; i++) {
+                StringBuilder sb = new StringBuilder();
+                for (int j=0; j<m; j++) {
+                    if (((1<<j) & b) > 0) sb.append(relation[i][j]).append(" ");
+                }
+                if(!set.add(sb.toString())) break;
+            }
+
+            if(set.size() == n) candidateKeyList.add(b);
+        }
+        return candidateKeyList.size();
     }
 
     public static void main(String[] args) {

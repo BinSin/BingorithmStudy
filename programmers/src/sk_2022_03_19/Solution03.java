dfe81b5284f392920ec319a8f09ace8bee50d1fb
BinSin <binyong318@gmail.com>
2022-03-19 17:00:38 +0900

    SK

---
+++ programmers/src/sk_2022_03_19/Solution03.java
@@ -0,0 +1,14 @@
+package sk_2022_03_19;
+
+public class Solution03 {
+
+    public static int solution(int[][] a, int[][] b, int m) {
+        int answer = -1;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ programmers/src/sk_2022_03_19/Solution04.java
@@ -0,0 +1,14 @@
+package sk_2022_03_19;
+
+public class Solution04 {
+
+    public static int[] solution(int n, int m, int k, int[][] records) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}

4cb4f11df54dc3f726abe568dbbb9db174ff4c61
BinSin <binyong318@gmail.com>
2022-03-25 19:16:09 +0900

    1. Calendar -> LocalDate 로 변경 2. 날짜 차이 계산은 ChronoUnit.DAYS.between 으로 계산

--- programmers/src/kakaobank_2022_03_19/Solution01.java
+++ programmers/src/kakaobank_2022_03_19/Solution01.java
@@ -1,34 +1,46 @@
 package kakaobank_2022_03_19;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 
 public class Solution01 {
 
     private static final int finalDay = 365;
-    private static final Calendar lastDate = new GregorianCalendar(2022, Calendar.DECEMBER, 31); // GregorianCalendar 룰 사용하여 날짜 관리
+
+    // 오래된 Date와 Calendar 클래스에는 생각보다 많은 문제점이 있다.
+    // 자바 버전업이 되면서 Date 클래스의 많은 메서드가 Deprecate 되었다.
+    // 가장 혼동스러운건 월 표기를 위한 상수 값을 확인하면 1월이 0 이어서 월 값에 1을 빼야한다.
+    // 이 때문에 가독성이 나빠진다.
+    // 그리고 Calendar 혹은 Date 클래스는 불변 객체가 아니어서
+    // 생성된 객체가 다른 코드에서도 공유되기 때문에 변경될 수 있다.
+    // JDK 1.8 버전에서 LocalDateTime 과 타임존 개념까지 포함할 수 있는 ZonedDateTime 이 추가되었고
+    // LocalDate 혹은 LocalDateTime 으로 이전보다 안전하고 편하게 날짜를 계산할 수 있게 되었다.
+    private static final LocalDate lastDate = LocalDate.of(2022, 12, 31);
+
 
     // 결제 정보 클래스
     private static class TransInfo {
-        Calendar date;
+        LocalDate date;
         int interestRate, amount;
 
-        public TransInfo(Calendar date, int interestRate, int amount) {
+        public TransInfo(LocalDate date, int interestRate, int amount) {
             this.date = date;
             this.interestRate = interestRate;
             this.amount = amount;
         }
     }
 
-    // 날짜를 일 단위로 바꿔주는 메서드
-    private static Calendar convertToDay(String dateStr) {
+    // 날짜를 GregorianCalendar로 바꿔주는 메서드
+    private static LocalDate convertToLocalDate(String dateStr) {
         String[] dateSplit = dateStr.split("/");
-        return new GregorianCalendar(2022, Integer.parseInt(dateSplit[0]) - 1, Integer.parseInt(dateSplit[1]));
+        return LocalDate.of(2022, Integer.parseInt(dateSplit[0]), Integer.parseInt(dateSplit[1]));
     }
 
     // 이자 구하는 메서드
-    private static int getWithdrawalAmount(Calendar sDate, Calendar eDate, int calAmount, int lastInterestRate) {
-        int diffDays = (int) (((eDate.getTimeInMillis() - sDate.getTimeInMillis()) / 1000) / (24*60*60)); // GregorianCalendar 를 사용하여 편하게 일 단위로 계산
-        return (calAmount * lastInterestRate) * diffDays / finalDay / 100;
+    private static int getWithdrawalAmount(LocalDate sDate, LocalDate eDate, int calAmount, int lastInterestRate) {
+        long diffDays = ChronoUnit.DAYS.between(sDate, eDate); // ChronoUnit 으로 두 날짜 사이의 간격을 구할 수 있다.
+        return (int) ((calAmount * lastInterestRate) * diffDays / finalDay / 100);
     }
 
 
@@ -40,18 +52,17 @@ public class Solution01 {
         for (String ledger : ledgers) {
             StringTokenizer st = new StringTokenizer(ledger);
             // 변수 정리
-            Calendar date = convertToDay(st.nextToken());
+            LocalDate date = convertToLocalDate(st.nextToken());
             int interestRate = Integer.parseInt(st.nextToken());
             int amount = Integer.parseInt(st.nextToken());
 
             if (amount > 0) { // 입금이면 stack에 추가
-                
                 TransInfo transInfo = new TransInfo(date, interestRate, amount);
                 s.add(transInfo);
             } else { // 출금이면 이자 계산 시작
                 while (amount < 0) { // 모두 출금될 때까지 반복 
                     TransInfo lastTrans = s.pop();
-                    Calendar lastDate = lastTrans.date;
+                    LocalDate lastDate = lastTrans.date;
                     int lastInterestRate = lastTrans.interestRate;
                     int lastAmount = lastTrans.amount;
 

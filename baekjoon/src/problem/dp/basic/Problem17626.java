906a69978be2449103cc967c4c8befbe6da93be7
BinSin <binyong318@gmail.com>
2022-09-06 23:32:17 +0900

    Four Squares

---
+++ baekjoon/src/problem/dp/basic/Problem17626.java
@@ -0,0 +1,30 @@
+package problem.dp.basic;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem17626 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+1];
+        for (int i=1; i<=n; i++) dp[i] = i;
+
+        List<Integer> squareNumberList = new ArrayList<>();
+        for (int i=2; i*i<=n; i++) squareNumberList.add(i*i);
+
+        for (int squreNumber : squareNumberList) {
+            for (int i=squreNumber; i<=n; i++) {
+                dp[i] = Math.min(dp[i - squreNumber] + 1, dp[i]);
+            }
+        }
+
+        bw.write(dp[n] + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}

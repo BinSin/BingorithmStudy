3f518e652f2f1486b6ecb217c43dcfc0c8cf2d02
BinSin <binyong318@gmail.com>
2022-10-31 23:58:55 +0900

    합승 택시 요금 1. 다익스트라 알고리즘 활용

---
+++ programmers/src/practice/kakao/level3/Solution08.java
@@ -0,0 +1,77 @@
+/*
+BinSin
+https://school.programmers.co.kr/learn/courses/30/lessons/72413
+분류: 다익스트라 알고리즘
+ */
+
+package practice.kakao.level3;
+
+import java.util.*;
+
+public class Solution08 {
+
+    private static final int MAX_FARE = 100001;
+
+    private static class Node {
+        int index, fare;
+
+        Node(int index, int fare) {
+            this.index = index;
+            this.fare = fare;
+        }
+    }
+
+    private static void dijkstra(int s, int[] dist, List<List<Node>> graph, int n) {
+        Queue<Node> q = new PriorityQueue<>(Comparator.comparingInt(o -> o.fare));
+        q.add(new Node(s, 0)); // 시작 정점
+        dist[s] = 0;
+
+        while (!q.isEmpty()) {
+            Node node = q.remove();
+
+            if (dist[node.index] < node.fare) continue;
+
+            List<Node> aroundNodeList = graph.get(node.index);
+            for (Node nextNode : aroundNodeList) {
+                if (dist[nextNode.index] <= node.fare + nextNode.fare) continue;
+                dist[nextNode.index] = node.fare + nextNode.fare;
+                q.add(new Node(nextNode.index, dist[nextNode.index]));
+            }
+        }
+    }
+
+    public static int solution(int n, int s, int a, int b, int[][] fares) {
+        List<List<Node>> graph = new ArrayList<>();
+        for (int i=0; i<=n; i++) graph.add(new ArrayList<>(i));
+        for (int[] fare : fares) {
+            int start = fare[0];
+            int end = fare[1];
+            int cost = fare[2];
+            graph.get(start).add(new Node(end, cost));
+            graph.get(end).add(new Node(start, cost));
+        }
+
+        int[] dist = new int[n+1]; // 정점 카운터
+        int[] distA = new int[n+1];
+        int[] distB = new int[n+1];
+
+        Arrays.fill(dist, MAX_FARE);
+        Arrays.fill(distA, MAX_FARE);
+        Arrays.fill(distB, MAX_FARE);
+
+        dijkstra(s, dist, graph, n);
+        dijkstra(a, distA, graph, n);
+        dijkstra(b, distB, graph, n);
+
+        int answer = Integer.MAX_VALUE;
+        for (int i=1; i<=n; i++) answer = Math.min(answer, dist[i] + distA[i] + distB[i]);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 6, s = 4, a = 6, b = 2;
+        int[][] fares = {{4,1,10}, {3, 5, 24}, {5, 6, 2}, {3, 1, 41}, {5, 1, 24}, {4, 6, 50}, {2, 4, 66}, {2, 3, 22}, {1, 6, 25}};
+        System.out.println(solution(n, s, a, b, fares));
+    }
+
+}

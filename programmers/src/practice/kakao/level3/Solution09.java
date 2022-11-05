cd43d11ed21c4ee1fa0d9e150f2a6640a76beeb1
binsin <binyong318@gmail.com>
2022-11-05 18:16:04 +0900

    징검다리 건너기 1. 이분 탐색으로 접근 2. 건널 수 있는 친구들의 수를 이분 탐색

--- programmers/src/practice/kakao/level3/Solution09.java
+++ programmers/src/practice/kakao/level3/Solution09.java
@@ -1,18 +1,42 @@
+/*
+BinSin
+https://school.programmers.co.kr/learn/courses/30/lessons/64062
+분류; 이분 탐색
+ */
+
 package practice.kakao.level3;
 
 public class Solution09 {
 
-    private static final int MAX_STONE_VALUE = 200001;
+    private static final int MAX_STONE_VALUE = 200000001;
+
+    private static boolean canCrossBride(int[] stones, int k, int countOfFriends) {
+        int count = 0;
+        for (int stone : stones) {
+            if (stone - countOfFriends >= 0) count = 0; // 건널 수 있음
+            else count++; // 건널 수 없음
+
+            if (count == k) return false;
+        }
+        return true;
+    }
 
     public static int solution(int[] stones, int k) {
-        int length = stones.length;
-        int min = MAX_STONE_VALUE;
-        for (int i=0; i<length - k + 1; i++) {
-            int max = 0;
-            for (int j=i; j<i+k; j++) max = Math.max(max, stones[j]);
-            min = Math.min(min, max);
+        int min = 1;
+        int max = MAX_STONE_VALUE;
+
+        int answer = 0;
+        while(min <= max) {
+            int mid = (min + max) / 2;
+            if (canCrossBride(stones ,k, mid)) {
+                answer = Math.max(answer, mid);
+                min = mid + 1;
+            } else {
+                max = mid - 1;
+            }
         }
-        return min;
+
+        return answer;
     }
 
     public static void main(String[] args) {
